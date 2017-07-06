76f5c6722163e810aba58bece944d1f972196ce6
aajungroth <aajungroth@ucdavis.edu>
2017-07-06 14:29:25 -0700

    Added persistent sessions that are stored in the mlab database

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ var mongoose = require('mongoose');
 
 //change name after we change the placeholder.js
 var db = require('./db/config');
+console.log('db', db);
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
+
+var store = new MongoDBStore({
+  uri: db.mongooseURI,
+  collection: 'sessions'
+});
+
+// Error handling
+store.on('error', function(error) {
+  assert.ifError(error);
+  assert.ok(false);
+  console.errror(error);
+});
 
 // see routes line 144 for description
 var specialHashRegex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
@@ -14,6 +29,15 @@ var specialHashRegex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}));
 
 app.use(require('./routes'));
 
