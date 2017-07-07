21ddbd1ddfc460b814b6d56b599b1b6c3defb551
johnmrox <johnmrox@gmail.com>
2017-07-07 14:51:51 -0400

    styling changes and static navbar

--- server.js
+++ server.js
@@ -10,33 +10,12 @@ var db = require('./db/config');
 var session = require('express-session');
 var MongoDBStore = require('connect-mongodb-session')(session);
 
-var store = new MongoDBStore({
-  uri: db.mongooseURI,
-  collection: 'sessions'
-});
-
-// Error handling
-store.on('error', function(error) {
-  assert.ifError(error);
-  assert.ok(false);
-  console.errror(error);
-});
-
-// see end of routes.js for description
+// see routes line 144 for description
 var specialHashRegex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
-app.use(session({
-  secret: 'This is a secret',
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7
-  },
-  store: store,
-  resave: true,
-  saveUninitialized: true
-}));
 
 app.use(require('./routes'));
 
